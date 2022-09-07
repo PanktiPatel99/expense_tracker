23c1d82992c0dc12b71c97d0af4a496aee06621c
PanktiPatel99 <panktip.mobio@gmail.com>
2022-09-07 16:48:23 +0530

    implement search filter functionality

--- src/Components/Transaction/ExpenseItems.js
+++ src/Components/Transaction/ExpenseItems.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItems.css";
 
 import expenseData from "../Expenses/ExpenseData";
 
 const ExpenseItems = () => {
+  const [searchItems, setSearchItems] = useState("");
+
+  const searchHandle = (e) => {
+    setSearchItems(e.target.value);
+    // console.log(e.target.value);
+    // console.log(searchItems);
+  };
+
+  let expenseDataSearch = expenseData.filter((item) => {
+    return Object.keys(item).some((newItem) => {
+      return item[newItem]
+        .toString()
+        .toLowerCase()
+        .includes(searchItems.toString().toLowerCase());
+    });
+  });
+  // console.log(expenseDataSearch);
+
   return (
     <div className="ExpenseItems">
+      <div className="search">
+        <p>Transaction Items</p>
+        <input
+          onChange={searchHandle}
+          value={searchItems}
+          placeholder="Search"
+          type="text"
+        />
+      </div>
+
       <table>
         <thead>
           <tr>
@@ -15,7 +43,7 @@ const ExpenseItems = () => {
           </tr>
         </thead>
         <tbody>
-          {expenseData
+          {expenseDataSearch
             .sort((a, b) => {
               return a.amount - b.amount;
             })
